17526f65b182790d155d7d425d8744bd72616a4e
minhtan <ttannn1104@gmail.com>
2025-11-11 21:56:41 +0700

    fe/admin ori

--- src/pages/admin/dashboard/AdminDashboard.tsx
+++ src/pages/admin/dashboard/AdminDashboard.tsx
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from 'react';
-import { Sidebar } from './ItemDashboard/SideBar';
-import { HeaderDashBoard } from './ItemDashboard/HeaderDashBoard';
-import { RevenueTab } from './ItemDashboard/mainDashBoard/Revenue';
-import { AccessoryTab } from './ItemDashboard/mainDashBoard/Accessory';
-import { UsersTab } from './ItemDashboard/mainDashBoard/UserContract';
-import { SellTab } from './ItemDashboard/mainDashBoard/SellCar';
-import { PurchaseTab } from './ItemDashboard/mainDashBoard/Purchased';
-=======
 import React, { useEffect, useState } from "react";
 import { Sidebar } from "./ItemDashboard/SideBar";
 import { HeaderDashBoard } from "./ItemDashboard/HeaderDashBoard";
 
 import { Accessories } from "./ItemDashboard/mainDashBoard/Accessories/index";
->>>>>>> feat/auth
 
 enum ActiveTab {
   REVENUE = 0,
@@ -21,31 +10,11 @@ enum ActiveTab {
   ACCESSORY = 2,
   FORSELL = 3,
   PURCHASE = 4,
+  ACCESSORIES = 5,
 }
 
 const AdminDashboard: React.FC = () => {
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
-<<<<<<< HEAD
-  const [activeTab, setActiveTab] = useState(ActiveTab.REVENUE);
-  const ActiveTabContent = React.useCallback(({ activeTabIdx }: { activeTabIdx: ActiveTab }) => {
-    switch (activeTabIdx) {
-      case ActiveTab.REVENUE:
-        return <RevenueTab />;
-      case ActiveTab.ACCESSORY:
-        return <AccessoryTab />;
-      case ActiveTab.USERS:
-        return <UsersTab />;
-      case ActiveTab.FORSELL:
-        return <SellTab />;
-      case ActiveTab.PURCHASE:
-        return <PurchaseTab />;
-      default:
-        return null;
-    }
-  },[activeTab]);
-  useEffect(()=>{},[])
-  
-=======
   const [activeTab, setActiveTab] = useState(ActiveTab.ACCESSORIES);
   const ActiveTabContent = React.useCallback(
     ({ activeTabIdx }: { activeTabIdx: ActiveTab }) => {
@@ -71,7 +40,6 @@ const AdminDashboard: React.FC = () => {
     []
   );
   useEffect(() => {}, []);
->>>>>>> feat/auth
 
   return (
     <div className="min-h-screen w-full relative inline-flex bg-gray-50">
