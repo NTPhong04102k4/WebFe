03ac7d7a83c2e03f8751afd6b803e645f9cec20a
NTPhong <phongnguyenphong267@gmail.com>
2026-05-13 21:39:48 +0700

    feat/hook for brandcar admin

--- src/shared/hooks/BrandCar/index.ts
+++ src/shared/hooks/BrandCar/index.ts
@@ -1,20 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
-import { brandCarRouteFn } from "src/services/api/functions/BrandCar/Routes.Fn";
-import { BrandCarResponse } from "src/shared/types/Reponse/Car";
+import { useBrandCarList } from "src/query/brand-car/useBrandCarQueries";
 
 export const useBrandCar = () => {
-  const { data, isLoading, error, refetch, isFetching, isError } = useQuery<
-    BrandCarResponse[],
-    Error
-  >({
-    queryKey: ["brand-cars"],
-    queryFn: async () => {
-      const res = await brandCarRouteFn.getBrandsCars();
-      return res;
-    },
-    staleTime: 5 * 60 * 1000,
-    retry: 1,
-  });
+  const { data, isLoading, error, refetch, isFetching, isError } =
+    useBrandCarList();
 
   return {
     brandCar: data ?? [],
