fbdadca06a01c50561708a53fd41013afd7d1a29
NTPhong <phongnguyenphong267@gmail.com>
2026-05-13 23:38:44 +0700

    feat/brand-accesories

--- src/query/brand-accessory/useBrandAccessoryQueries.ts
+++ src/query/brand-accessory/useBrandAccessoryQueries.ts
@@ -1,5 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { brandRouteFn } from "src/services/api/functions/accessories/brand/Route.Fn";
+import type {
+  BrandRequestCreate,
+  BrandRequestUpdate,
+} from "src/shared/types/Request/accessories/brand";
 import { brandAccessoryKeys } from "./keys";
 
 export function useBrandAccessoryList() {
@@ -8,4 +12,20 @@ export function useBrandAccessoryList() {
     queryFn: () => brandRouteFn.getBrands(),
     staleTime: 5 * 60_000,
   });
+}
+
+export function useBrandAccessoryMutations() {
+  const qc = useQueryClient();
+  const invalidate = () => qc.invalidateQueries({ queryKey: brandAccessoryKeys.all });
+
+  return {
+    createBrandAccessory: useMutation({
+      mutationFn: (body: BrandRequestCreate) => brandRouteFn.createBrand(body),
+      onSuccess: invalidate,
+    }),
+    updateBrandAccessory: useMutation({
+      mutationFn: (body: BrandRequestUpdate) => brandRouteFn.updateBrand(body),
+      onSuccess: invalidate,
+    }),
+  };
 }
